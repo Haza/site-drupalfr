15976ed2bbce8e3ea10e59c27e0a071ef2652dc3
Nicolas Meyer <nicolas@happyculture.coop>
2014-02-03 00:58:51 +0100

    First, basic, implementation of filtering event, using meetup API.

--- www7/sites/all/modules/custom/drupalfr/drupalfr_events/drupalfr-events-listing.tpl.php
+++ www7/sites/all/modules/custom/drupalfr/drupalfr_events/drupalfr-events-listing.tpl.php
@@ -1,7 +1,8 @@
 <div class="<?php print $classes; ?>">
+<?php print $venues; ?>
 <?php foreach($events as $event): ?>
   <div class="drupalfr-event-details drupalfr-event-<?php print $display_mode; ?>">
-  <?php if ($display_mode == 'teaser'): ?>
+    <?php if ($display_mode == 'teaser'): ?>
     <p>
       <span class="event-name"><?php print l($event['name'], $event['event_url']); ?></span><br />
       <span class="event-date"><?php print format_date($event['time'] / 1000, 'short'); ?></span> //
